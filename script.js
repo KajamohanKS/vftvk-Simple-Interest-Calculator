e1482ae568741bbe1aac5cfaddd87248a8c84392
Krishnadashan Kajamohan <54034874+KajamohanKS@users.noreply.github.com>
2021-06-23 12:13:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+/**
+ * Kajamohan.K
+ * Simple Interest Calculator
+ * JavaScript File
+ */
+
+// Display the Rate value
+function showRate() {
+    var printRateVal = document.getElementById("rateVal");
+    var disRate = '<b><i>' + document.getElementById("rate").value + '%<\/i><\/b>';
+    printRateVal.innerHTML = disRate;
 }
-        
+
+// Calculate and Display the Interest Value
+function calInterest() {
+    setResults();
+    var rate = document.getElementById("rate").value;
+    var amount = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+
+    var interest = parseFloat(rate) * parseFloat(amount) * parseFloat(years) / 100;
+
+    var finalYear = new Date().getFullYear() + parseInt(years);
+
+    var op = "Your Deposit : <mark> " + amount + " <\/mark>" +
+        "<br>Interest Rate : <mark> " + rate + "% <\/mark>" +
+        "<br>Interest You will recive an Amount : <mark> " + parseInt(interest) + " <\/mark>" +
+        "<br>in the Year : <mark> " + finalYear + " <\/mark>";
+
+    if (checkValue()) {
+        document.getElementById("result").innerHTML = op;
+
+    } else {
+        document.getElementById("result").innerText = "Check the Value!!!!<br>Enter a Positive Number";
+    }
+}
+
+// Check the Rate, Principal and No.of Years is Valid or Not
+function checkValue() {
+    var rate = document.getElementById("rate").value;
+    var amount = document.getElementById("principal").value;
+    var years = document.getElementById("years").value;
+
+    if (rate > 0 && amount > 0 && years > 0) {
+        return true;
+    } else {            // input Values are lessthan Zero or not Numbers
+        return false;
+    }
+}
+
+// Reset the Rate, Principal and No.of Years value to Zero
+function resetAll() {
+    document.getElementById("rate").value = 0;
+    document.getElementById("principal").value = 0;
+    document.getElementById("years").value = 0;
+    showRate();
+    var res = document.getElementById("result");
+    res.removeAttribute("class");
+    res.innerHTML = "";
+}
+
+// Display the Result usig add class Attribute
+function setResults() {
+    var res = document.getElementById("result");
+    res.setAttribute("class", "resultShow");
+}
